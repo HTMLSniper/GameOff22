c7f517f9d4609186f8a08fe84a6e6f2b2741eb26
Colin Vavra <c.vavra@web.de>
2022-11-29 20:30:05 +0100

    fix time

--- GlowPixel.gd
+++ GlowPixel.gd
@@ -4,8 +4,10 @@ onready var timer = $Timer
 onready var anim = $AnimationPlayer
 
 func _ready() -> void:
-	timer.wait_time = rand_range(1,3)
+	timer.wait_time = rand_range(1,15)
+	timer.start()
 
 func _on_Timer_timeout() -> void:
-	timer.wait_time = rand_range(1,3)
+	timer.wait_time = rand_range(1,15)
 	anim.play("pulse")
+	timer.start()
