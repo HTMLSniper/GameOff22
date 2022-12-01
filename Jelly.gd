ab62ced77cf5fde1804dce28dea261c3aafac00a
Deleteur1 <heizmaja@dhbw-loerrach.de>
2022-12-01 10:05:58 +0100

    change animals

--- Jelly.gd
+++ Jelly.gd
@@ -7,16 +7,15 @@ onready var follow = $Path2D/PathFollow2D
 
 var inc = 0
 var speed=10
-var min_scale_factor = 1.3
+var min_scale_factor = 16
 var scale_factor = 2.5
-var max_scale_factor = 35 # int 
+var max_scale_factor = 32 # int
 
 func _ready() -> void:
 	sprite.play("move")
 	inc = randi() % 800
-	scale_factor = (randi() % max_scale_factor) / 10
-	if scale_factor <= min_scale_factor: 
-		scale_factor += 1
+	follow.offset = inc
+	scale_factor = rand_range(min_scale_factor,max_scale_factor) / 10
 	sprite.scale.x = scale_factor
 	sprite.scale.y = scale_factor
 
