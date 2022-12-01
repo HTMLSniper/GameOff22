f691c80dcfc4e40fd633caa391706b118ccc4ec5
Deleteur1 <heizmaja@dhbw-loerrach.de>
2022-12-01 14:12:54 +0100

    add splash ink

--- Octupus.gd
+++ Octupus.gd
@@ -4,13 +4,17 @@ onready var sprite = $Path2D/PathFollow2D/AnimatedSprite
 onready var follow = $Path2D/PathFollow2D
 onready var shadow_right = $Path2D/PathFollow2D/AnimatedSprite/Right_LightOccluder
 onready var shadow_left = $Path2D/PathFollow2D/AnimatedSprite/Left_LightOccluder
+onready var make_ink = $AnimatedSprite
+onready var timer = $Timer
 
+export var ink_on = true
 
 var inc = 0
 var speed = 25
 var min_scale_factor = 16
 var scale_factor = 2.5
 var max_scale_factor = 31 # int
+var maxdistance_to_sprite = 75
 
 func _ready() -> void:
 	sprite.play("move")
@@ -37,4 +41,15 @@ func _process(delta: float) -> void:
 		shadow_left.visible = false
 	else:
 		pass
-	
+	make_ink.global_position.x = 0
+	make_ink.global_position.y = Global.player.position.y
+	if ink_on == true:
+		if sprite.global_position.distance_to(Global.player.position) < maxdistance_to_sprite:
+			if timer.is_stopped() == true:
+				make_ink.set_frame(0)
+				timer.start()
+			make_ink.visible = true
+
+func _on_Timer_timeout():
+	make_ink.visible = false
+	make_ink.set_frame(0)
