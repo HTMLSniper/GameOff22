47ba95e19952f04a3afdfc8ac647908cdfce463c
Colin Vavra <c.vavra@web.de>
2022-12-01 10:01:39 +0100

    add coin sound

--- Coin.gd
+++ Coin.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 onready var animation = $AnimationPlayer
+onready var pickup_sound = $AudioStreamPlayer 
 
 export(int) var number = 1
 
 var collected = false
 
 func _ready() -> void:
+	Global.connect("sound_changed_live", self, "sound_changed")
 	if number >= len(Global.coins):
 		Global.coins.append(collected)
 	if Global.coins[number]:
@@ -14,6 +16,13 @@ func _ready() -> void:
 
 func _on_Coin_body_entered(body: Node) -> void:
 	if body.has_method("get_coin"):
+		pickup_sound.pitch_scale = rand_range(0.8,1.2)
+		animation.play("coin_found")
 		body.get_coin()
 		Global.coins[number] = true
-		queue_free()
+
+func sound_changed():
+	if Global.sound_on:
+		pickup_sound.volume_db = Global.sound_volume - 15
+	else:
+		pickup_sound.volume_db = -80
