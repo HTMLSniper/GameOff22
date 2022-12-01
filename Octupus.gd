d5640011e864b2a4a936f54437b03a938bd6ea39
Colin Vavra <c.vavra@web.de>
2022-12-01 11:11:11 +0100

    fix helligkeit

--- Octupus.gd
+++ Octupus.gd
@@ -4,7 +4,9 @@ onready var sprite = $Path2D/PathFollow2D/AnimatedSprite
 onready var follow = $Path2D/PathFollow2D
 onready var shadow_right = $Path2D/PathFollow2D/AnimatedSprite/Right_LightOccluder
 onready var shadow_left = $Path2D/PathFollow2D/AnimatedSprite/Left_LightOccluder
+onready var light = $Path2D/PathFollow2D/AnimatedSprite/Light2D
 
+export var lightintensity_circle = 0.5
 
 var inc = 0
 var speed = 25
@@ -19,6 +21,7 @@ func _ready() -> void:
 	scale_factor = rand_range(min_scale_factor,max_scale_factor) / 10
 	sprite.scale.x = scale_factor
 	sprite.scale.y = scale_factor
+	light.energy = lightintensity_circle
 
 func _process(delta: float) -> void:
 	sprite.play("move")
